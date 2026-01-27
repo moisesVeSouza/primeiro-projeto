102ab4a61c79668dcfba9d12e39221d3d4a629c9
moisesVeSouza <mvs338409@hotmail.com>
2026-01-27 07:51:05 -0300

    fix: Working with DTO and Omit.

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -6,6 +6,12 @@ import AppointmentsRepository from '../repositories/AppointmentsRepository';
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
 
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+});
+
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
@@ -20,7 +26,10 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ message: 'This appointment is already booked' });
   }
 
-  const appointment = appointmentsRepository.create(provider, parseDate);
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parseDate,
+  });
 
   return response.json(appointment);
 });

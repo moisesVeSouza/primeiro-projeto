a88c2d73338e1a04865edd6d01ef15a7160febdf
moisesVeSouza <mvs338409@hotmail.com>
2026-03-22 08:43:31 -0300

    feat: Create users model, update appointmens model

--- src/database/migrations/1774097033608-CreateAppointments.ts
+++ src/database/migrations/1774097033608-CreateAppointments.ts
@@ -16,12 +16,20 @@ export default class CreateAppointments1774097033608 implements MigrationInterfa
           {
             name: 'provider',
             type: 'varchar',
-            isNullable: false,
           },
           {
             name: 'date',
             type: 'timestamp with time zone',
-            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()'
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()'
           },
         ],
       }),
